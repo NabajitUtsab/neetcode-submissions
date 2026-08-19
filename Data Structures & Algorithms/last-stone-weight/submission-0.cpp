7914721b1695aa4430d5778456f9307171e32d01
Nabajit Dey <88704676+NabajitUtsab@users.noreply.github.com>
2026-08-19 11:59:39 +0600

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+
+        for (int i = 0; i < stones.size(); i++) {
+            pq.push(stones[i]);
+        }
+
+        while (pq.size() > 1) {
+            int x = pq.top();
+            pq.pop();
+
+            int y = pq.top();
+            pq.pop();
+
+            int d = abs(x - y);
+
+            if (x != y) {
+                pq.push(d);
+            }
+        }
+        if (pq.empty()) {
+            return 0;
+        }
+        return pq.top();
+    }
+};
